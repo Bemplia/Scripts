f755c27cc81be3f70f45bfc1daf6c4995faf4e6b
Bemplia <145816318+Bemplia@users.noreply.github.com>
2023-09-23 16:41:38 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,30 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+
+local Window = Library.CreateLib("Bemplia scripts", "RJTheme4")
+
+local Scripts = Window:NewTab("Scripts")
+local Gui = Window:NewTab("Gui")
+
+local ScriptsSection = Scripts:NewSection("Scripts")
+local GuiSection = Gui:NewSection("Gui")
+
+ScriptsSection:NewButton("The rake Noob Edition", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bemplia/The-rake-Noob-Edition-script/main/script.lua", true))()
+end)
+
+ScriptsSection:NewButton("Prison Life", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bemplia/Prison-life/main/script.lua", true))()
+end)
+
+ScriptsSection:NewButton("Tower of hell", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bemplia/Tower/main/script.lua", true))()
+end)
+
+ScriptsSection:NewButton("Pet Simulator X", "", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bemplia/Pet-simulator/main/script.lua", true))()
+end)
+
+GuiSection:NewKeybind("Toggle GUI", "", Enum.KeyCode.L, function()
+	Library:ToggleUI()
+end)
